99ab2984b444ff9c76138a495f77e7e5c195c3c1
Matthew Cain <yooksi@zoho.com>
2016-10-09 18:05:42 +0200

    Fix RegularBow animation issue
    Resolves: #1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,13 @@ All notable changes to this mod project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/) and uses a custom versioning system.
 
+## Version 0.1.4 (2016-10-09)
+### Fixed
+- Resolved: [#1](https://github.com/yooksi/Better-Archery-Reborn/issues/1) RegularBow model animation issue
+
+### Changed
+- .gitignore has been updated with more rules.
+
 ## Version 0.1.3 (2016-10-09)
 ### Added
 - A new bow type, regular bow.
--- src/main/java/com/yooksi/betterarchery/item/RegularBow.java
+++ src/main/java/com/yooksi/betterarchery/item/RegularBow.java
@@ -1,8 +1,17 @@
 package com.yooksi.betterarchery.item;
 
+import javax.annotation.Nullable;
+
 import com.yooksi.betterarchery.common.BetterArchery;
 
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.ItemBow;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class RegularBow extends ItemBow
 {
@@ -15,5 +24,30 @@ public class RegularBow extends ItemBow
 		
         setCreativeTab(BetterArchery.tabBetterArchery);
         localInstance = this;
+	
+        this.addPropertyOverride(new ResourceLocation("pull"), new IItemPropertyGetter()
+        {
+        	@SideOnly(Side.CLIENT)
+            public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn)
+            {
+                if (entityIn == null)
+                    return 0.0F;
+                
+                else
+                {
+                    ItemStack itemstack = entityIn.getActiveItemStack();
+                    return itemstack != null && itemstack.getItem() == localInstance ? (float)(stack.getMaxItemUseDuration() - entityIn.getItemInUseCount()) / 20.0F : 0.0F;
+                }
+            }
+        });
+        this.addPropertyOverride(new ResourceLocation("pulling"), new IItemPropertyGetter()
+        {
+        
+        	@SideOnly(Side.CLIENT)
+            public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn)
+            {
+                return entityIn != null && entityIn.isHandActive() && entityIn.getActiveItemStack() == stack ? 1.0F : 0.0F;
+            }
+        });
 	}
 }
