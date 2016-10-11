1dcfda70bfbac6e7de1e278cab7fa21f6b681304
Matthew Cain <yooksi@zoho.com>
2016-10-11 03:26:06 +0200

    Implement bow arrow velocity controller
    Makes it possible to modify arrow velocity when shot from our custom
    bow.

--- src/main/java/com/yooksi/betterarchery/item/ArchersBow.java
+++ src/main/java/com/yooksi/betterarchery/item/ArchersBow.java
@@ -4,13 +4,21 @@ import javax.annotation.Nullable;
 
 import com.yooksi.betterarchery.common.BetterArchery;
 
-import net.minecraft.entity.Entity;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.init.Enchantments;
+import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.IItemPropertyGetter;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemArrow;
 import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
+import net.minecraft.stats.StatList;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -21,8 +29,11 @@ import net.minecraftforge.fml.relauncher.SideOnly;
  */
 public abstract class ArchersBow<I extends ArchersBow> extends ItemBow
 {	
-	/** This multiplier modifies the duration of the bows pulling animation.  */
+	/** This multiplier modifies the duration of the bows pulling animation. */
 	protected float pullingSpeedMult = 1.0F;
+	
+	/** This multiplier modifies the speed of arrow being shot from this bow. */
+	protected float arrowSpeedMult = 1.0F;
 	
 	/** 
 	 * Perform custom bow initialization after it's been created. <br>  
@@ -76,6 +87,121 @@ public abstract class ArchersBow<I extends ArchersBow> extends ItemBow
             }
         });
 	}
+	
+	/**
+	 * Try to find ammunition this bow can fire in player's possesion. <br>
+	 * First check <b>OFF_HAND</b> and <b>MAIN_HAND</b> then go through the inventory.
+	 * 
+	 * @return ItemStack of the ammo or <code>null</code> if no ammo was found
+	 */
+	private ItemStack findAmmo(EntityPlayer player)   
+	{
+		if (this.isArrow(player.getHeldItem(EnumHand.OFF_HAND)))
+	    {
+			return player.getHeldItem(EnumHand.OFF_HAND);
+	    }
+	    else if (this.isArrow(player.getHeldItem(EnumHand.MAIN_HAND)))
+	    {
+	        return player.getHeldItem(EnumHand.MAIN_HAND);
+	    }
+	    else
+	    {
+	        for (int i = 0; i < player.inventory.getSizeInventory(); ++i)
+	        {
+	        	ItemStack itemstack = player.inventory.getStackInSlot(i);
+
+	            if (this.isArrow(itemstack))
+	                return itemstack;
+	        }
+	         
+	        return null;
+	    }
+	}
+	
+	@Override
+	public void onPlayerStoppedUsing(ItemStack stack, World worldIn, EntityLivingBase entityLiving, int timeLeft)
+    {
+        if (entityLiving instanceof EntityPlayer)
+        {
+            EntityPlayer entityplayer = (EntityPlayer)entityLiving;
+            boolean flag = entityplayer.capabilities.isCreativeMode || EnchantmentHelper.getEnchantmentLevel(Enchantments.INFINITY, stack) > 0;
+            ItemStack itemstack = this.findAmmo(entityplayer);
+
+            int i = this.getMaxItemUseDuration(stack) - timeLeft;
+            i = net.minecraftforge.event.ForgeEventFactory.onArrowLoose(stack, worldIn, (EntityPlayer)entityLiving, i, itemstack != null || flag);
+            if (i < 0) return;
+
+            if (itemstack != null || flag)
+            {
+                if (itemstack == null)
+                {
+                    itemstack = new ItemStack(Items.ARROW);
+                }
+
+                // Vanilla arrow velocity value ranges from 0.0F to 1.0F.               
+                float f = getArrowVelocity(i) * arrowSpeedMult;
+                
+                if ((double)f >= 0.1D)
+                {
+                    boolean flag1 = entityplayer.capabilities.isCreativeMode || (itemstack.getItem() instanceof ItemArrow ? ((ItemArrow)itemstack.getItem()).isInfinite(itemstack, stack, entityplayer) : false);
+
+                    if (!worldIn.isRemote)
+                    {
+                        ItemArrow itemarrow = (ItemArrow)((ItemArrow)(itemstack.getItem() instanceof ItemArrow ? itemstack.getItem() : Items.ARROW));
+                        EntityArrow entityarrow = itemarrow.createArrow(worldIn, itemstack, entityplayer);
+                        entityarrow.setAim(entityplayer, entityplayer.rotationPitch, entityplayer.rotationYaw, 0.0F, f * 3.0F, 1.0F);
+
+                        if (f >= 1.0F)
+                        {
+                            entityarrow.setIsCritical(true);
+                        }
+
+                        int j = EnchantmentHelper.getEnchantmentLevel(Enchantments.POWER, stack);
+
+                        if (j > 0)
+                        {
+                            entityarrow.setDamage(entityarrow.getDamage() + (double)j * 0.5D + 0.5D);
+                        }
+
+                        int k = EnchantmentHelper.getEnchantmentLevel(Enchantments.PUNCH, stack);
+
+                        if (k > 0)
+                        {
+                            entityarrow.setKnockbackStrength(k);
+                        }
+
+                        if (EnchantmentHelper.getEnchantmentLevel(Enchantments.FLAME, stack) > 0)
+                        {
+                            entityarrow.setFire(100);
+                        }
+
+                        stack.damageItem(1, entityplayer);
+
+                        if (flag1)
+                        {
+                            entityarrow.pickupStatus = EntityArrow.PickupStatus.CREATIVE_ONLY;
+                        }
+
+                        worldIn.spawnEntityInWorld(entityarrow);
+                    }
+
+                    worldIn.playSound((EntityPlayer)null, entityplayer.posX, entityplayer.posY, entityplayer.posZ, SoundEvents.ENTITY_ARROW_SHOOT, SoundCategory.NEUTRAL, 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 1.2F) + f * 0.5F);
+
+                    if (!flag1)
+                    {
+                        --itemstack.stackSize;
+
+                        if (itemstack.stackSize == 0)
+                        {
+                            entityplayer.inventory.deleteStack(itemstack);
+                        }
+                    }
+
+                    entityplayer.addStat(StatList.getObjectUseStats(this));
+                }
+            }
+        }
+    }
 	
 	/**
 	 * Get the current progress of the bow's pulling animation for entity. <br>
