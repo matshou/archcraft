47280807fa16d7bacb6fd647e6767b0be44e68d3
Matthew Cain <yooksi@zoho.com>
2016-10-16 19:46:58 +0200

    Remove redundant variant entry

--- src/main/java/com/yooksi/betterarchery/item/ArchersBow.java
+++ src/main/java/com/yooksi/betterarchery/item/ArchersBow.java
@@ -58,8 +58,7 @@ public abstract class ArchersBow extends ItemBow
 	public enum BowItemVariant
 	{
 		BOW_PLAIN("bow_plain", null), 
-		BOW_CLOTH_GRIP("bow_with_grip", null),
-		
+
 		BOW_WOOLEN_GRIP("bow_with_grip", new Color(255, 255, 255)),
 		BOW_LEATHER_GRIP("bow_with_grip", new Color(107, 46, 22));
 
