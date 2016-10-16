a43bbc272c2dc9ba405300c21cf821611febd005
Matthew Cain <yooksi@zoho.com>
2016-10-16 19:46:43 +0200

    Add bow tooltip info
    Simple and recurve bow now have variant-sensitive tooltip information
    displayed in-game.

--- src/main/java/com/yooksi/betterarchery/item/RecurveBow.java
+++ src/main/java/com/yooksi/betterarchery/item/RecurveBow.java
@@ -1,5 +1,10 @@
 package com.yooksi.betterarchery.item;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
 public class RecurveBow extends ArchersBow
 {
 	public RecurveBow(BowItemVariant variant)
@@ -10,5 +15,25 @@ public class RecurveBow extends ArchersBow
 		
 		pullingSpeedMult = 0.85F;
 		arrowSpeedMult = 1.20F;
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer player, java.util.List<String> info, boolean par4) 
+	{		
+		info.add(com.mojang.realmsclient.gui.ChatFormatting.ITALIC + "A swift, true-firing bow that almost aims itself.");
+		
+		switch(ArchersBow.getBowItemVariant(stack.getItem())) {
+
+		case BOW_LEATHER_GRIP:
+			info.add(com.mojang.realmsclient.gui.ChatFormatting.ITALIC + "Equipped with a leather grip.");
+			break;
+			
+		case BOW_WOOLEN_GRIP:	
+			info.add(com.mojang.realmsclient.gui.ChatFormatting.ITALIC + "Equipped with a woolen grip.");
+		
+		default: 
+			break;   // BOW_PLAIN or null 	
+		}	
 	}
 }
--- src/main/java/com/yooksi/betterarchery/item/SimpleBow.java
+++ src/main/java/com/yooksi/betterarchery/item/SimpleBow.java
@@ -1,9 +1,34 @@
 package com.yooksi.betterarchery.item;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
 public class SimpleBow extends ArchersBow
 {
 	public SimpleBow(BowItemVariant variant)
 	{
 		super(variant);
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer player, java.util.List<String> info, boolean par4) 
+	{		
+		info.add(com.mojang.realmsclient.gui.ChatFormatting.ITALIC + "Such simple, so right.");
+		
+		switch(ArchersBow.getBowItemVariant(stack.getItem())) {
+
+		case BOW_LEATHER_GRIP:
+			info.add(com.mojang.realmsclient.gui.ChatFormatting.ITALIC + "With a leather grip.");
+			break;
+			
+		case BOW_WOOLEN_GRIP:	
+			info.add(com.mojang.realmsclient.gui.ChatFormatting.ITALIC + "With a woolen grip.");
+		
+		default: 
+			break;   // BOW_PLAIN or null 	
+		}	
 	}
 }
