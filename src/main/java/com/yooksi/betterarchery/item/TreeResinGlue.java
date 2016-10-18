dd66c368e5e3740687f5adbfb92252e72bad10a4
Matthew Cain <yooksi@zoho.com>
2016-10-18 23:31:46 +0200

    Add TreeResinGlue to creativeTabs

--- src/main/java/com/yooksi/betterarchery/item/TreeResinGlue.java
+++ src/main/java/com/yooksi/betterarchery/item/TreeResinGlue.java
@@ -1,8 +1,13 @@
 package com.yooksi.betterarchery.item;
 
+import com.yooksi.betterarchery.common.BetterArchery;
+
 import net.minecraft.item.Item;
 
 public class TreeResinGlue extends Item
 {
-
+	public TreeResinGlue()
+	{
+		this.setCreativeTab(BetterArchery.creativeTab);
+	}
 }
