229efffa81cce671e0a0787f3041909105b1e56d
Matthew Cain <yooksi@zoho.com>
2020-04-15 09:32:58 +0200

    Implement item registry function

--- src/main/java/yooksi/archcraft/util/RegistryHandler.java
+++ src/main/java/yooksi/archcraft/util/RegistryHandler.java
@@ -12,5 +12,11 @@ public class RegistryHandler {
 	@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD, modid="archcraft")
 	public static class Events {
 
+		@SubscribeEvent
+		public static void onItemsRegistry(final RegistryEvent.Register<Item> event) {
+
+			ArchCraft.LOGGER.info("Registering mod items...");
+			event.getRegistry().registerAll(ModItems.get());
+		}
 	}
 }
