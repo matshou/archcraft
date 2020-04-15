fd637c3b12b1e0763659a1ecbe64ff8f65c7c3ca
Matthew Cain <yooksi@zoho.com>
2020-04-15 09:32:48 +0200

    Create empty registry handler class

---
+++ src/main/java/yooksi/archcraft/util/RegistryHandler.java
@@ -0,0 +1,16 @@
+package yooksi.archcraft.util;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import yooksi.archcraft.ArchCraft;
+import yooksi.archcraft.init.ModItems;
+
+public class RegistryHandler {
+
+	@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD, modid="archcraft")
+	public static class Events {
+
+	}
+}
